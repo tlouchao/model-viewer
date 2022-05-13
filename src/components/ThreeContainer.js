d8d3fb5d0a0590fc379c280653e126d685e9bb4b
Tiffany <tiffanylouchao@gmail.com>
2022-05-12 22:35:18 -0400

    fix canvas css

--- src/components/ThreeContainer.js
+++ src/components/ThreeContainer.js
@@ -5,9 +5,10 @@ import { THREE_BGCOLOR } from "./../constants/constants"
 class ThreeContainer extends React.Component {
     constructor(props){
         super(props)
-        this.ref = React.createRef()
+        this.ref = null
         this.animate = this.animate.bind(this)
         this.handleResize = this.handleResize.bind(this)
+        this.setCanvasRef = this.setCanvasRef.bind(this)
         this.state = {
             geometry: new THREE.BoxGeometry( 1, 1, 1 ),
             material: new THREE.MeshBasicMaterial( { color: 0x00ff00 } ),
@@ -21,29 +22,33 @@ class ThreeContainer extends React.Component {
         }
     }
     componentDidMount() {
-        this.setState({width: this.ref.current.clientWidth,
-                       height: window.innerHeight,
-                       camera: new THREE.PerspectiveCamera( 75, 
-                            this.ref.current.clientWidth/window.innerHeight, 
-                            0.1, 
-                            1000 ),
-                       cube: new THREE.Mesh(this.state.geometry, this.state.material)},
-                      () => {
-                        this.state.scene.background = new THREE.Color(THREE_BGCOLOR)
-                        this.state.scene.add(this.state.cube)
-                        this.state.renderer.setSize( this.state.width, this.state.height)
-                        this.ref.current.appendChild( this.state.renderer.domElement )
-                        this.state.camera.position.z = 3
-                        this.animate()
-                    })
+        if (this.ref){
+            this.setState({width: this.ref.clientWidth,
+                        height: window.innerHeight,
+                        camera: new THREE.PerspectiveCamera( 75, 
+                                this.ref.clientWidth/window.innerHeight,
+                                0.1, 
+                                1000 ),
+                        cube: new THREE.Mesh(this.state.geometry, this.state.material)},
+                        () => {
+                            this.state.scene.background = new THREE.Color(THREE_BGCOLOR)
+                            this.state.scene.add(this.state.cube)
+                            this.state.renderer.setSize( this.state.width, this.state.height)
+                            this.ref.appendChild( this.state.renderer.domElement )
+                            this.state.camera.position.z = 3
+                            this.animate()
+                        })
+        }
         this.handleResize();
         window.addEventListener('resize', this.handleResize);
     }
 
     componentWillUnmount() {
         cancelAnimationFrame(this.state.frameId);
-        while (this.ref.current.lastChild) {
-            this.ref.current.removeChild(this.ref.current.lastChild)
+        if (this.ref){
+            while (this.ref.lastChild) {
+                this.ref.removeChild(this.ref.lastChild)
+            }
         }
         window.removeEventListener('resize', this.handleResize)
     }
@@ -56,18 +61,24 @@ class ThreeContainer extends React.Component {
     };
 
     handleResize() {
-        this.setState({width: this.ref.current.clientWidth,
-                       height: window.innerHeight},
-                     () => {
-                        this.state.renderer.setSize(this.state.width, this.state.height)  
-                        this.state.camera.aspect = this.state.width / this.state.height;
-                        this.state.camera.updateProjectionMatrix()       
-                     })
+        if (this.ref) {
+            this.setState({width: this.ref.clientWidth,
+                        height: window.innerHeight},
+                        () => {
+                            this.state.renderer.setSize(this.state.width, this.state.height)  
+                            this.state.camera.aspect = this.state.width / this.state.height;
+                            this.state.camera.updateProjectionMatrix()       
+                        })
+        }
     }
+
+    setCanvasRef = element => {
+      this.ref = element;
+    };
 
     render() {
         return (
-            <div id="three-canvas" ref={this.ref} />
+            <div id="three-canvas" ref={this.setCanvasRef} />
         )
     }
 }
