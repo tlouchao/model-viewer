4ca069a5ace36f6be2f91fd1fd86680cc96edf58
Tiffany <tiffanylouchao@gmail.com>
2022-05-15 02:36:38 -0400

    webpack argv

--- webpack.config.js
+++ webpack.config.js
@@ -4,57 +4,63 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 dotenv.config()
 
-module.exports = {
-  mode: 'development',
-  entry: "./src/index.js",
-  output: {
-  path: path.resolve(__dirname, 'build'),
-  filename: 'bundle.js'
-},
-devServer: {
-  port: 3000
-},
-resolve: {
-  extensions: ['.js', 'jsx', 'sass', 'scss', '.svg', '.png']
-},
-module: {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
+module.exports = (env, argv) => {
+  console.log(argv.mode + '\n')
+  const mode = argv.mode || 'development'
+  const config = {
+    mode: 'development',
+    devtool: (mode === 'development') ? 'inline-source-map' : false,
+    entry: "./src/index.js",
+    output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
     },
-    {
-      test: /\.s[ac]ss$/,
-      exclude: /node_modules/,
-      use: ['style-loader', 'css-loader', 'sass-loader']
+    devServer: {
+      port: 3000
     },
-    {
-      test: /\.png$/,
-      exclude: /node_modules/,
-      use: [
+    resolve: {
+      extensions: ['.js', 'jsx', 'sass', 'scss', '.svg', '.png']
+    },
+    module: {
+      rules: [
         {
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-          },
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
         },
-      ],
+        {
+          test: /\.s[ac]ss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.png$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 8192,
+              },
+            },
+          ],
+        },
+      ]
     },
-  ]
-},
-plugins: [
-  new CopyWebpackPlugin({
-    patterns: [
-    { from: `${__dirname}/public`, to: 'public' },
-    { from: `${__dirname}/src/static/imgs`, to: 'public/assets' }
-    ]
-  }),
-  new HtmlWebpackPlugin({ 
-    template: './src/index.html',
-    inject: 'body'
-  })
-  ]
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+        { from: `${__dirname}/public`, to: 'public' },
+        { from: `${__dirname}/src/static/imgs`, to: 'public/assets' }
+        ]
+      }),
+      new HtmlWebpackPlugin({ 
+        template: './src/index.html',
+        inject: 'body'
+      })
+      ]
+    }
+  return config
 }
